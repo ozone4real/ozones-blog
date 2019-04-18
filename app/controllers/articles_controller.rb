199c961cca194d1501d0ca06ea5ab7252836484b
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-04-18 18:30:22 +0100

    create article template and action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,16 @@
 class ArticlesController < ApplicationController
+  def new
+    
+  end
+
+  def create
+    @article = Article.new(article_params)
+    redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
