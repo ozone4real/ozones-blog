6bb1817f666bcfce778e539dbb2b20ee8c49dd97
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-05-08 18:40:08 +0100

    modify user image display method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def gravatar_for(user)
+  def gravatar_for(user, size='')
     gravatar_id = Digest::MD5::hexdigest(user.email)
     gravatar_url = user.image_url.to_s.empty? ? "https://secure.gravatar.com/avatar/#{gravatar_id}" : user.image_url.to_s
-    image_tag(gravatar_url, alt: user.username, class:"profile-img rounded-circle")  
+    image_tag(gravatar_url, alt: user.username, class:"profile-img#{size} rounded-circle")  
   end
 end
