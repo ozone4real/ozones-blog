3cffc099323fbfeade7ed404501a68c4492222d5
Ezenwa Ogbonna <ezenwaogbonna@Ezenwas-MacBook-Pro.local>
2021-04-14 04:11:36 +0100

    fix bug

--- app/jobs/user_job.rb
+++
@@ -1,9 +0,0 @@
-class UserJob < ApplicationJob
-  pubsubeque_options retries: 4
-
-  def perform(email, username)
-    User.create!(email: email, username: username, password: "ohthree", full_name: "Ezenwa Ogbonna")
-    (1..1000000).to_a
-    user = User.all
-  end
-end
